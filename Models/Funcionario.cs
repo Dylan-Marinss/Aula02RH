41cd135b5b4532264dff99e518511358401446b2
Dylan-Marinss <dylanmarins24@gmail.com>
2025-08-06 22:32:10 -0300

    Aula02cs

---
+++ Models/Funcionario.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aula02RH.Models.Enuns;
+
+namespace Aula02RH.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; } //prop + enter
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataAdmissão { get; set; }
+        public decimal Salario { get; set; }
+        public TipoFuncionarioEnum TipoFuncionario { get; set; }
+
+        public void ReajustarSalario()
+        {
+            Salario = Salario + (Salario * 10 /100);
+        }
+
+        public string ExibirPeriodoExperiencia()
+        {
+            string periodoExperiencia = 
+                string.Format("Periodo de Experiência: {0} até {1}", DataAdmissão, DataAdmissão.AddMonths(3));
+                return periodoExperiencia;
+        }
+
+        public decimal CalcularDescontoVT(decimal percentual)
+        {
+            decimal desconto = this.Salario * percentual/100;
+            return desconto;
+        }
+    }
+}
